58e4685a2bc48b011b290fa078ef80b972bb1142
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-23 17:35:01 +0200

    .

---
+++ T26-Spring-Mysql-Ex3/src/main/java/com/juan/ex3/T26SpringMysqlEx3Application.java
@@ -0,0 +1,13 @@
+package com.juan.ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T26SpringMysqlEx3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T26SpringMysqlEx3Application.class, args);
+	}
+
+}
---
+++ T26-Spring-Mysql-Ex3/src/test/java/com/juan/ex3/T26SpringMysqlEx3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T26SpringMysqlEx3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
